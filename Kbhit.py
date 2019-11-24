05a81956ef18da74e1ab778054002b4b0f032738
Luiza <luizacpacheco@poli.ufrj.br>
2019-11-24 16:53:15 -0300

    Classe KBHit separada

---
+++ Kbhit.py
@@ -0,0 +1,71 @@
+#Autores: Luiza Pacheco, Victor assis, Alexandre Salem e Marco Gomes
+#Data de inicio: 04/10/2019
+#
+#Descrição: Jogo Trilha em Python
+
+#######################  Comandos Sistema Operacional  #########################
+import os
+
+# Windows
+if os.name == 'nt':
+    import msvcrt
+
+# Posix (Linux, OS X)
+else:
+    import curses
+    import sys
+    import termios
+    TERMIOS = termios
+    import atexit
+    from select import select
+    def getkey():
+        fd = sys.stdin.fileno()
+        new = termios.tcgetattr(fd)
+        new[3] = new[3] & ~TERMIOS.ICANON & ~TERMIOS.ECHO
+        new[6][TERMIOS.VMIN] = 1
+        new[6][TERMIOS.VTIME] = 0
+        termios.tcsetattr(fd, TERMIOS.TCSANOW, new)
+        c = None
+        try:
+            c = os.read(fd, 1)
+        finally:
+            return (str(c)[2])
+
+class KBHit:
+
+    def set_normal_term(self):
+        ''' Resets to normal terminal.  On Windows this is a no-op.
+        '''
+        
+        if os.name == 'nt':
+            pass
+        
+        else:
+            termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.old_term)
+
+
+
+    def getch(self):
+        ''' Returns a keyboard character after kbhit() has been called.
+            Should not be called in the same program as getarrow().
+        '''
+        
+        s = ''
+        
+        if os.name == 'nt':
+            return msvcrt.getch().decode('utf-8')
+        
+        else:
+            return sys.stdin.read(1)
+    
+
+    def kbhit(self):
+        ''' Returns True if keyboard character was hit, False otherwise.
+        '''
+        if os.name == 'nt':
+            return msvcrt.kbhit()
+        
+        else:
+            dr,dw,de = select([sys.stdin], [], [], 0)
+            return dr != []
+        
